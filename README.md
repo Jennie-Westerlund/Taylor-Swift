6f797544eb8708d82f6e32d1269a0d28e54f6684
Jennie-Westerlund <jmn.westerlund@gmail.com>
2024-12-05 21:53:48 +0100

    Added JS event.listener and buttons in html

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # API
 API assignment
+
+(Need)To do:
+* Add pictures
+* Add over all styling 
+
+
+(Nice)To do:
+* Error-handeling 
+* Loading indicator 
--- script.js
+++ script.js
@@ -1,19 +1,55 @@
 /* const url = 'https://potterapi-fedeperin.vercel.app/en/'; */
+/* const url = 'https://api.kanye.rest'; */
+/* const url = 'https://taylorswiftapi.onrender.com/get'; */
 
-const url = 'https://api.kanye.rest';
+/* const url = `https://taylorswiftapi.onrender.com/get${album}`; */
 const quoteBox = document.querySelector('.quoteBox');
 
-fetch(url)
+
+const getAlbum = (album) => {
+  const clickedButton = album.target;
+  const clickedButtonValue = clickedButton.getAttribute("data-album-name");
+  console.log(clickedButtonValue);
+  const url = `https://taylorswiftapi.onrender.com/get?album=${clickedButtonValue}`;
+  fetch(url)
   .then((response) => {
     // We take the response Promise and return it as JSON.
-    console.log(response)
     return response.json();
   })
   .then((json) => {
     // Now we can now use the JSON as a normal object.
-    console.log(json);
+    const cleanQuote = json.quote.replace("/", "<br>");
+    quoteBox.innerHTML = `
+    <h2>${cleanQuote}</h2>
+    <p>Song: ${json.song}<p>
+  `; 
   });
-  
+};
+
+const albumButtons = document.querySelectorAll('.albumButton');
+
+// Loop through the buttons and attach event listeners
+albumButtons.forEach((button) => {
+  button.addEventListener('click', getAlbum);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   const fetchQuote = async (quote) => {
     try {
