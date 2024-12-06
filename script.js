9f46984ec631d7d645a4b8332b1e24cff58e759b
Jennie-Westerlund <jmn.westerlund@gmail.com>
2024-12-06 11:30:49 +0100

    Safer .textContent and added images and some css

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 /* const url = 'https://taylorswiftapi.onrender.com/get'; */
 
 /* const url = `https://taylorswiftapi.onrender.com/get${album}`; */
-const quoteBox = document.querySelector('.quoteBox');
+const quoteBox = document.querySelector('.quote-box');
+const quote = document.querySelector('.quote');
+const songTitle = document.querySelector('.song-title');
 
 
 const getAlbum = (album) => {
-  const clickedButton = album.target;
+  const clickedButton = album.target.parentElement;
   const clickedButtonValue = clickedButton.getAttribute("data-album-name");
   console.log(clickedButtonValue);
   const url = `https://taylorswiftapi.onrender.com/get?album=${clickedButtonValue}`;
@@ -18,15 +20,13 @@ const getAlbum = (album) => {
   })
   .then((json) => {
     // Now we can now use the JSON as a normal object.
-    const cleanQuote = json.quote.replace("/", "<br>");
-    quoteBox.innerHTML = `
-    <h2>${cleanQuote}</h2>
-    <p>Song: ${json.song}<p>
-  `; 
+    const cleanQuote = json.quote.replace(/\//g, '\n');
+    quote.textContent = `${cleanQuote}`;
+    songTitle.textContent = `Song: ${json.song}`; 
   });
 };
 
-const albumButtons = document.querySelectorAll('.albumButton');
+const albumButtons = document.querySelectorAll('.album-button');
 
 // Loop through the buttons and attach event listeners
 albumButtons.forEach((button) => {
