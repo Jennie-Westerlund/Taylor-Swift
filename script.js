519a1bdbcc51083e1b30300fbe84bc4b7697049a
Jennie-Westerlund <jmn.westerlund@gmail.com>
2024-12-13 19:40:28 +0100

    Removed old out-commented code

--- script.js
+++ script.js
@@ -27,33 +27,3 @@ const albumButtons = document.querySelectorAll('.album-button');
 albumButtons.forEach((button) => {
   button.addEventListener('click', getAlbum);
 });
-
-
-/* Ändrar hover designen till click vid touch  */
-
-/* if (window.matchMedia('(hover: none) and (pointer: coarse)').matches) {
-  const albumButtons = document.querySelectorAll('.album-button');
-  
-  albumButtons.forEach(button => {
-      button.addEventListener('click', () => {
-          // Reset all album-name opacities
-          buttons.forEach(btn => {
-              const name = btn.querySelector('.album-name');
-              name.style.opacity = '0';
-              btn.querySelector('.album-cover').style.opacity = '1'; // Reset cover opacity
-          });
-
-          // Toggle the clicked button's album-name opacity
-          const name = button.querySelector('.album-name');
-          const cover = button.querySelector('.album-cover');
-
-          if (name.style.opacity === '1') {
-              name.style.opacity = '0';
-              cover.style.opacity = '1'; // Reset to fully visible
-          } else {
-              name.style.opacity = '1';
-              cover.style.opacity = '0.2'; // Apply low opacity
-          }
-      });
-  });
-} */
